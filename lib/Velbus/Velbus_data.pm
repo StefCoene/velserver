43e5eae2a7579c1e7b9d2ca1bc85efca5c6b26fd
Stef Coene <stef.coene@docum.org>
2018-12-01 15:19:20 +0100

    remove unused code

--- lib/Velbus/Velbus_data.pm
+++ lib/Velbus/Velbus_data.pm
@@ -9,14 +9,4 @@ $global{Cons}{Days}{4} = "friday" ;
 $global{Cons}{Days}{5} = "saturday" ;
 $global{Cons}{Days}{6} = "sunday" ;
 
-# Convert the Channel/Button names found when a Button/Input is pressed, make a simple lookup table
-$global{Cons}{ConvertButton}{'01'} = "01" ;
-$global{Cons}{ConvertButton}{'02'} = "02" ;
-$global{Cons}{ConvertButton}{'04'} = "03" ;
-$global{Cons}{ConvertButton}{'08'} = "04" ;
-$global{Cons}{ConvertButton}{'10'} = "05" ;
-$global{Cons}{ConvertButton}{'11'} = "06" ;
-$global{Cons}{ConvertButton}{'12'} = "07" ;
-$global{Cons}{ConvertButton}{'14'} = "08" ;
-
 return 1 ;
