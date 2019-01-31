a97839582977903ad4341a1367eed46d2392fa0c
Stef Coene <stef.coene@docum.org>
2019-01-31 23:12:33 +0100

    some tests to set the color on edge-lit touche panels

--- bin/commands.pl
+++ bin/commands.pl
@@ -46,6 +46,48 @@ if ( ! defined $sock ) {
       &scan($sock) ;
    } elsif ( $global{opts}{option} eq "status" ) {
       &update_module_status($sock) ;
+   } elsif ( $global{opts}{option} =~ /test(..)(..)(..)/ ) {
+      my $red   = $1 ; # 0 - 255
+      my $green = $2 ; # 0 - 255
+      my $blue  = $3 ; # 0 - 255
+
+      my $sat = "10" ;
+
+      my $address = "14" ;
+
+      my $max = bin_to_dec ("1111111") ;
+      #print "$max\n" ;
+      #print "$sat\n" ;
+      $sat = $sat * $max / 100  ;
+      #print "$sat\n" ;
+      $sat = hex_to_bin(dec_to_hex ($sat)) ;
+      #print "$sat\n" ;
+      $sat =~ s/^.//g ;
+      #print "$sat\n" ;
+      $sat = "0" . $sat ;  # RGB, 1 = white
+      #print "$sat\n" ;
+      my $sat = &bin_to_hex ($sat) ;
+      #print "$sat\n" ;
+      my $pal = "30" ;
+      $pal = &dec_to_hex ($pal) ;
+      &send_message ($sock, $address, "D4", $pal, $sat, $red, $green, $blue) ;
+      $pal = hex_to_dec($pal) ;
+      $sat = hex_to_bin($sat) ;
+      $red = hex_to_dec($red) ;
+      $green = hex_to_dec($green) ;
+      $blue = hex_to_dec($blue) ;
+      print "$pal, $sat, $red, $green, $blue\n" ;
+
+      my $hex2 = &bin_to_hex ("01111111") ; # default + apply to all
+      my $hex2 = &bin_to_hex ("11111111") ; # custom + apply to all
+
+      my $hex3 = &bin_to_hex ("11111111") ; # everywhere  BRTL
+
+      $pal = hex_to_bin(dec_to_hex ($pal)) ;
+      $pal =~ s/^...//g ;
+      my $hex4 = &bin_to_hex ("011$pal") ; #
+      &send_message ($sock, $address, "D4", $hex2, $hex3, $hex4) ;
+
    } elsif ( $global{opts}{option} eq "tempinterval" ) {
       &set_temperature_interval_all($sock,"60") ;
    } elsif ( $global{opts}{option} eq "date" ) {
